e11445b5ecc066ff62a7ea5b4969abc35a32779a
b <fltw11ck@gmail.com>
2025-02-12 14:44:49 +0300

    Add files via upload

---
+++ WebApplication2/Program.cs
@@ -0,0 +1,14 @@
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//middleware ekleme (güvenlik)
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("istek alındı." + context.Request.Path);
+    await next();
+    Console.WriteLine("yanıt gönderildi.");
+});
+
+app.MapGet("/", () => "merhaba asp.net core!");
+app.Run();
---
+++ WebApplication2/middleware/Class.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication2.middleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine("istek alındı." + context.Request.Path);
+            await _next(context);
+            Console.WriteLine("yanıt gönderildi.");
+        }
+    }
+}
