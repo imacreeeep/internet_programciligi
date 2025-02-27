03327ed7ebb706c4241c3c3b48ac73b2a1e5de71
b <fltw11ck@gmail.com>
2025-02-27 10:45:05 +0300

    Add files via upload

---
+++ MovieApp/MovieApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace MovieApp.Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			string filmBasligi = "film başlığı";
+			string filmAciklama = "film açıklaması";
+			string filmYonetmen = "film yönetmen adı";
+			string[] oyuncular = { "oyuncu 1", "oyuncu 2" };
+
+			ViewBag.FilmBasligi = filmBasligi;
+			ViewBag.FilmAciklama = filmAciklama;
+			ViewBag.FilmYonetmen = filmYonetmen;
+			ViewBag.Oyuncular = oyuncular;
+
+			return View();
+		}
+		
+		public IActionResult About()
+		{
+			return View();
+		}
+	}
+}
---
+++ MovieApp/MovieApp.Web/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApp.Web.Controllers
+{
+	public class MoviesController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult List()
+		{
+			return View("Movies");
+		}
+		public IActionResult Details()
+		{
+			return View();
+		}
+	}
+}
