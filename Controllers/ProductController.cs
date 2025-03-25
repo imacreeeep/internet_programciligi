9c788beed746133a48b03a6a7b96811dc5aeaa29
b <fltw11ck@gmail.com>
2025-03-25 11:58:36 +0300

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using intpr_uyg1.Models;
+using System.Collections.Generic;
+
+
+namespace intpr_uyg1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product { Id = 1, Name = "telefon", Price = 100000 },
+                new Product { Id = 2, Name = "tablet", Price = 15000 },
+                new Product { Id  =3, Name = "mouse", Price = 1000 }
+            };
+            return View(products);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace intpr_uyg1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
